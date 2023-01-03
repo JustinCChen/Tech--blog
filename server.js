9b4d7678e7910828eacc27030799a6e56401b580
JustinCChen <justinchen1211@gmail.com>
2023-01-02 16:17:32 -0800

    userRoutes

--- server.js
+++ server.js
@@ -12,36 +12,32 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
+        expiration: 1000 * 60 * 30 // will expire after 30 minutes
     })
-  };
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.use(session(sess));
-
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(routes);
 
-app.use(require('./controllers/'));
+sequelize.sync();
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+    console.log(`App listening on port ${PORT}!`);
 });
